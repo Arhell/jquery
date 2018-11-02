acf0762093d574f4e2e84790b99dfce80ec2d982
isychevskyi <arhell333@gmail.com>
2018-11-02 13:00:37 +0200

    upd

--- accordion/main.js
+++ accordion/main.js
@@ -1,25 +1,29 @@
-var multiply = true; // Эти переменные относятся к Аккордиону - переси их в тело функции accordion
-var titleSelector = $('.accordion-title');
-
 function accordion(mode) {
-  if (mode == multiply) { // Используй тройное равно для сравнения
+
+  var titleSelector = $('.accordion-title');
+
+  mode = mode || 'single';
+
+  if (mode === 'multiply') {
     titleSelector.on('click', function () {
-      if($(this).parent().hasClass('active')) { // У тебя по коду несколько раз повторяется $(this).parent() . Закешируй это в переменную с понятным именем и используй ее. А то непонятно что это за элемент да и дублируется код
-        $(this).parent().removeClass('active');
+
+      var itemWrapper = $(this).parent();
+
+      if(itemWrapper.hasClass('active')) {
+        itemWrapper.removeClass('active');
       }
 
       else {
-        $(this).parent().addClass('active');
+        itemWrapper.addClass('active');
       }
-    })
+    });
   }
 
-  else {
+  else if(mode === 'single'){
     titleSelector.on('click', function () {
       $(this).next().toggle();
-    })
+    });
   }
 };
 
-// тебе нужно передавать не переменную, а строку (и в функции сравнивать со строкой)
-accordion(multiply);
+accordion('multiply');
