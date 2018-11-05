628ee49b4af6032a74f9427e2ebb2fcd12cb073a
Arhell <arhell333@gmail.com>
2018-11-06 00:32:02 +0200

    add change var

--- counter/main.js
+++ counter/main.js
@@ -1,15 +1,16 @@
 $( document ).ready(function() {
   var initialValue = $('.counter-value').text();
+  var changeValue = initialValue;
 
   $('.counter-plus').on('click', function () {
-    var plusCount = initialValue++ + 1;
+    var plusCount = changeValue++ + 1;
     var countValue = plusCount;
     $('.counter-value').text(countValue);
   })
 
   $('.counter-minus').on('click', function () {
     if (initialValue >= initialValue) {
-      var minusCount = initialValue-- - 1;
+      var minusCount = changeValue-- - 1;
       var countValue = minusCount;
       $('.counter-value').text(countValue);
     }
