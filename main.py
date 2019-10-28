d54d777a42424d161724796e53d8c08533bf2563
Arhell <arhell333@gmail.com>
2019-10-29 00:28:00 +0200

    vars

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ c = 'Hello world'
 if a > b:
   print(c)
   print(a + b)
+
+num_1 = input('Enter first num: ')
+num_2 = int (input('Enter second num: '))
+res = float (num_1) + num_2
+Res = input('Enter some test: ')
+Res += 3
+print(Res)
+print('result', res)
