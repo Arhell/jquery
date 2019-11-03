2f788267ba40b329b29b0b867b28532ed55c4a4f
Arhell <arhell333@gmail.com>
2019-11-04 00:51:08 +0200

    num

---
+++ l2.py
@@ -0,0 +1,22 @@
+print(2 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 2) #5.0
+print(30 % 2)
+print(3 // 2)
+
+print(7 % 2) #1
+print(7 // 2) #3
+
+print(-5 - 3)
+
+print(5 ** 2) #25
+print(5 ** 3) #125
+
+print(25 ** 0.5) #5.0
+
+print(2 + 3 * 4) #14
+print((2 + 3) * 4) #20
+
+print(0.5 + 1.5)
+print(0.1 + 0.2)
