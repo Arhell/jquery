f007e1ded4d5c9c72d0a55293550406534f2f557
Arhell <arhell333@gmail.com>
2019-11-07 00:19:04 +0200

    str

---
+++ l5.py
@@ -0,0 +1,20 @@
+# words = 'Some \'text\''
+words = 'Some \\text\''
+print(words)
+
+# verse = 'Some text\n\
+# Some text Some\n\
+# text Some text'
+
+verse = ('Some text\n' \
+        'Some text \n' \
+        'Some text Some text')
+
+print(verse)
+
+string = """\
+Some text
+Some text\
+"""
+
+print(string)
