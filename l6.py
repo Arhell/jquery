a87578bfc289bb7b571489a36d700bedcd109c18
Arhell <arhell333@gmail.com>
2019-11-08 00:45:57 +0200

    str2

---
+++ l6.py
@@ -0,0 +1,37 @@
+s = r'C:\d\new.txt' # row string
+print(s)
+
+a = 'py' 'thon'
+print(a)
+
+a1 = 'some, '
+a2 = 'text'
+a3 = a1 + a2
+print(a3)
+
+name = 'Text'
+age = 100
+print('Some text ' + name + ' Text ' + str(age))
+
+print('some' * 5)
+
+h = 'some text'
+print(h[0])
+print(h[-8])
+
+# j[0] = 'd'
+
+# [X:Y:Z]
+
+string = 'some text'
+print(string[0:7])
+print(string[-1])
+print(string[0:4])
+print(string[:4])
+print(string[2:])
+print(string[::2]) #sm et
+print(string[::-1]) #revert string
+print(string[:4] + string[5:]) #some text
+
+
+
