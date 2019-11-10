0939a4ab142362aabc517a7f66d5d97109c3c056
Arhell <arhell333@gmail.com>
2019-11-10 22:21:39 +0200

    str

---
+++ tutorial/l7.py
@@ -0,0 +1,17 @@
+s = 'some text'
+# print(len(s))
+s2 = s.capitalize()
+print(s, s2)
+print(s.center(20, '!'))
+print(s.count('e', 0, 20))
+
+a = 'some text'
+print(a.find('e'))
+print(a.index('e')) #index
+print(a.replace('e', 'a'))
+print(a.split())
+
+b = 'sometext'
+print(b.isdigit()) #false find number
+print(b.isalpha())
+
