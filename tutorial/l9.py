4c644276689425f38fd207da43c06cdcd0471771
Arhell <arhell333@gmail.com>
2019-11-16 19:32:08 +0200

    if

---
+++ tutorial/l9.py
@@ -0,0 +1,22 @@
+x = 1
+if x:
+  print('x = True')
+else:
+  print('x = False')
+
+light = 'red'
+if light == 'red':
+  print('Stop')
+elif light == 'yellow':
+  print('Wait')
+elif light == 'green':
+  print('Go')
+else:
+  print('Text')
+
+age = int(input('Some text num'))
+
+if age >= 18:
+  print('1')
+else:
+  print(f'{age} 2')
