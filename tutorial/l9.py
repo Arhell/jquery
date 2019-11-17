b43a4b970845515f2fc571888a9528c3ebc2743d
Arhell <arhell333@gmail.com>
2019-11-17 22:33:39 +0200

    if+

--- tutorial/l9.py
+++ tutorial/l9.py
@@ -20,3 +20,25 @@ if age >= 18:
   print('1')
 else:
   print(f'{age} 2')
+
+time = 12
+day = 'st'
+if time < 12 or time > 13:
+    print('Open')
+else:
+    print('Close')
+
+if time >= 8 and day != 'su':
+  print('Open')
+else:
+  print('Close')
+
+b = 1
+if not b:
+  print('ok')
+else:
+  print('no')
+
+a = 1
+res = 'ok' if a else 'no'
+print(res)
