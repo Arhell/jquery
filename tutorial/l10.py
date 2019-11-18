bb2da8b51ea56418e2c6286c69ec359146ce9305
Arhell <arhell333@gmail.com>
2019-11-19 01:09:42 +0200

    for

--- tutorial/l10.py
+++ tutorial/l10.py
@@ -2,3 +2,18 @@ i = 1
 while i <= 10:
   print(i, end=' ')
   i += 1
+
+s = 'Hello world'
+for l in s:
+  if l == ' ':
+    continue
+  print(f'"{l}"', end=' ')
+
+for i in 'Hello world':
+  if i == ' ':
+    break
+  print(i, end=' ')
+else:
+  print('\nNo spaces')
+
+
