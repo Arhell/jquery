0f290f4fbb88a01555eff72a13aef8c7cafcb7b5
Arhell <arhell333@gmail.com>
2019-11-22 01:14:34 +0200

    list+

--- tutorial/l11.py
+++ tutorial/l11.py
@@ -5,6 +5,15 @@ l3 = list((1, 2, 3))
 
 l4 = [i for i in 'hello']
 # l5 = [i for i in 'hello world' if i != ' ' and i != ['a']]
-l5 = [i for i in 'hello world' if i != ' ' and i not in ['a', 'e', 'i', 'o', 'u', ' ']]
+l5 = [i*2 for i in 'hello world' if i != ' ' and i not in ['a', 'e', 'i', 'o', 'u', ' ']]
 
 print(l, l2, l3, l4, l5, sep='\n')
+
+l6 = list(range(0, 11, 2))
+
+print(l6)
+
+for i in range(1, 3):
+  print(f'Out #{i}')
+  for j in range(1, 3):
+    print(f'In #{j}')
