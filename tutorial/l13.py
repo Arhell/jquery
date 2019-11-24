fcc21c2dabcf89bee1ba4e6fc620f9748257a1de
Arhell <arhell333@gmail.com>
2019-11-25 00:09:47 +0200

    list methods

--- tutorial/l13.py
+++ tutorial/l13.py
@@ -4,3 +4,19 @@ name = ['some', 'text', 'lorem']
 l[2] = 'world'
 l[:2] = [10, 15]
 print(l[0:3])
+
+l.append('new')
+l.extend([5, 7])
+l2 = ['hi', 19]
+l += l2
+l.insert(1, 'elem')
+# l.remove('world')
+el = l.pop(1)
+print(l, el)
+
+l3 = ['hello', 'hi', 'text', 'some']
+l3.sort()
+l3 = l3.sorted(l3)
+print(l3)
+
+print(l, l.count('world'))
