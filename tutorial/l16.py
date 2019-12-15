9d87071a906d30d65f3d55beeb1d9265aaeafb8f
Arhell <arhell333@gmail.com>
2019-12-15 21:40:56 +0200

    tuple next

--- tutorial/l16.py
+++ tutorial/l16.py
@@ -7,3 +7,22 @@ t5 = tuple('text')
 print(type(t1))
 print(t1.__sizeof__())
 
+
+t6 = tuple('some')
+t7 = tuple(' text')
+t8 = t6 + t7
+print(t8.count('l'))
+
+if 'l' in t8:
+  print(t8.index('l'))
+else:
+  print('text')
+
+for i in t8:
+  if i == ' ':
+    continue
+  print(f'"{i}"', end=' ')
+
+
+
+
