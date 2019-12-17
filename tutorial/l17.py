e82ca5be84cea1d57eaeaee32fb23130c32a1a69
Arhell <arhell333@gmail.com>
2019-12-18 00:20:38 +0200

    join

--- tutorial/l17.py
+++ tutorial/l17.py
@@ -10,3 +10,22 @@ print(palindromes)
 palindromes2 = [word for word in words if word == word[::-1]] # v2
 
 print(palindromes2)
+
+
+my_str = ['Око за око', 'А роза упала на лапу Азора', 'Около Миши молоко']
+palindromes3 = []
+for word in my_str:
+    word_r = word.replace(' ', '').lower()
+    if word_r == word_r[::-1]:
+        palindromes3.append(word)
+
+print(palindromes3)
+
+l = list(range(1, 10))
+l2 = list('hello')
+print(l)
+
+s = '-'.join(map(str, l))
+s2 = ','.join(l2)
+print(s)
+print(s2)
