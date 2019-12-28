fe62cc1f48d23a61604c6a8c9b0aa7dd9ddca1ce
Arhell <arhell333@gmail.com>
2019-12-28 20:31:03 +0200

    num

---
+++ tutorial/l21.py
@@ -0,0 +1,17 @@
+x = 33
+user_num = 0
+cnt = 0
+
+while True:
+  user_num = int(input('Some text some text'))
+  cnt += 1 # cnt = cnt + 1
+  if user_num == x:
+    print(f'Some text {cnt}')
+    print('some text')
+    break
+
+  elif user_num > x:
+    print('Some text')
+
+  else:
+    print('Some text')
