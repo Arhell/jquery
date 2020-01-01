4b5977fc7d6c56ab82909efbc3e3be410dba49cc
Arhell <arhell333@gmail.com>
2020-01-01 21:45:20 +0200

    docs

---
+++ tutorial/l24.py
@@ -0,0 +1,13 @@
+def get_sum(a, b):
+  """
+  :return sum args a anb b
+
+  :param a: first operand
+  :type a: int
+  :param b: second operand
+  :type b: int
+  :return: return type int
+  """
+  return a + b
+
+print(get_sum(1, 2))
