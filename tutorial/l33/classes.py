3a3e40813f4c3b553f37fbf6ac4014bb9ba79717
Arhell <arhell333@gmail.com>
2020-03-20 19:11:43 +0200

    constructor

---
+++ tutorial/l33/classes.py
@@ -0,0 +1,8 @@
+class Person:
+
+  def __init__(self, name):
+    self.name = name
+
+  def print_info(self):
+    print(f'Some text {self.name}')
+
---
+++ tutorial/l33/l33.py
@@ -0,0 +1,7 @@
+from classes import Person
+
+person1 = Person('Text')
+person1.print_info()
+
+person2 = Person('Some')
+person2.print_info()
