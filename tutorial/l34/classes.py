da7049e0d4714fe00c8fd4f068971110fb1abfaf
Arhell <arhell333@gmail.com>
2020-04-04 01:35:17 +0300

    parsing class

--- tutorial/l34/classes.py
+++ tutorial/l34/classes.py
@@ -8,21 +8,22 @@ class Person:
     print(f'Name {self.name}, Age: {self.__age}')
 
   def get_age(self):
-      return self.__age
+    return self.__age
 
-    def set_age(self, value):
-      if value in range(1, 101):
-        self.__age = value
-      else:
-        print("Wrpng age")
+  def set_age(self, value):
+    if value in range(1, 101):
+      self.__age = value
+    else:
+      print("Wrong age")
 
-    @property
-    def age(self):
-      return self.__age
+  @property
 
-    @age.setter
-    def age(self, value):
-      if value in range(1, 101):
-        self.__age = value
-      else:
-        print("Wrpng age")
+  def age(self):
+    return self.__age
+
+  @age.setter
+  def age(self, value):
+    if value in range(1, 101):
+      self.__age = value
+    else:
+      print("Wrong age")
