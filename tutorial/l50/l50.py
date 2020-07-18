6e0a0fdaff15dcad13fcf904eeebfa3bbe29ab93
Arhell <arhell333@gmail.com>
2020-07-18 15:22:37 +0300

    program 1

---
+++ tutorial/l50/l50.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+root = Tk()
+
+# def color_red():
+#     l['text'] = 'red'
+#     e.delete(0, END)
+#     e.insert(0, '#ff0000')
+
+# def color_orange():
+#     l['text'] = 'orange'
+#     e.delete(0, END)
+#     e.insert(0, '#ff7d00')
+#
+# def color_yellow():
+#     l['text'] = 'yellow'
+#     e.delete(0, END)
+#     e.insert(0, '#ffff00')
+
+def get_color(text_color, hex_color):
+    l['text'] = text_color
+    e.delete(0, END)
+    e.insert(0, hex_color)
+
+l = Label(root)
+e = Entry(root, width=30, justify='center')
+l.pack()
+e.pack()
+
+
+
+btn_red = Button(root, bg='#ff0000', command=lambda: get_color('red', '#ff0000')).pack(fill=X)
+btn_orange = Button(root, bg='#ff7d00', command=lambda: get_color('orange', '#ff7d00')).pack(fill=X)
+btn_yellow = Button(root, bg='#ffff00', command=lambda: get_color('yellow', '#ffff00')).pack(fill=X)
+# btn_orange = Button(root, bg='#ff7d00', command=color_orange).pack(fill=X)
+# btn_yellow = Button(root, bg='#ffff00', command=color_yellow).pack(fill=X)
+
+
+
+
+root.mainloop()
