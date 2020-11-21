eb804a1a462eaf81f124c3ed2a65ec43c4bfb7a0
Arhell <arhell333@gmail.com>
2020-11-21 19:27:51 +0200

    ttk next

--- tutorial/l63/l.py
+++ tutorial/l63/l.py
@@ -7,13 +7,24 @@ root.geometry('400x300+1000+300')
 s = ttk.Style()
 # print(s.theme_names())
 # s.theme_use('clam')
-s.configure('.', foreground="red")
+# s.configure('.', foreground="red")
+# s.configure('TButton', foreground="red")
+s.configure('TButton', padding=6)
+s.configure('red.TButton', padding=6)
 
 Button(root, text="Button 1", padx=40, pady=5).pack(pady=10)
-ttk.Button(root, text="Button 2", width=21).pack()
+ttk.Button(root, text="Button 2", width=20, style="red.TButton").pack()
+ttk.Button(root, text="Button 2", width=20).pack(pady=10)
 
 Entry(root).pack(pady=10)
 ttk.Entry(root).pack()
 
+def choose(event):
+  print(select.current(), select.get())
+
+select = ttk.Combobox(root, values=["1", "2", "3", "4", "5"])
+select.place(relx=0.5, rely=0.5, anchor=CENTER)
+select.current(0)
+select.bind("<<ComboboxSelected>>", choose)
 
 root.mainloop()
