b83853bd21cc731ef6e65608878a8f90f6f1a7a1
Arhell <arhell333@gmail.com>
2020-12-06 10:50:16 +0200

    app start

---
+++ tutorial/l65/l.py
@@ -0,0 +1,12 @@
+from tkinter import ttk
+from ttkthemes import ThemedTk
+import request
+from tkinter import messagebox
+
+API_KEY = ''
+API_URL = ''
+
+root = ThemedTk(theme="arc")
+root.geometry("500x400+1000+300")
+root.resizable(0, 0)
+
