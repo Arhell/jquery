dae769c5251ebfc7b4bc2694d2fd303eb3b6f306
Arhell <arhell333@gmail.com>
2020-12-12 02:43:42 +0200

    app next

--- tutorial/l65/l.py
+++ tutorial/l65/l.py
@@ -6,7 +6,38 @@ from tkinter import messagebox
 API_KEY = ''
 API_URL = ''
 
+def get_weather():
+    if not entry.get():
+        messagebox.showwarning('warning', 'Add')
+    else:
+        params = {
+            "appid": API_KEY,
+            "q": entry.get(),
+            "units": "metric",
+            "lang": "en"
+        }
+        r = request.get(API_URL, params=params)
+        weather = r.json()
+        
+
 root = ThemedTk(theme="arc")
 root.geometry("500x400+1000+300")
 root.resizable(0, 0)
+
+top_frame = ttk.Frame(root)
+top_frame.place(relx=0.5, rely=0.1, relwidth=0.5, relheigth=0.1, anchor='n')
+
+entry = ttk.Entry(top_frame)
+entry.place(relwidth=0.7, relheigth=1)
+
+button = ttk.Button(top_frame, text="Text", command=get_weather)
+button.place(relx=0.7, relwidth=0.3, relheigth=1)
+
+lower_frame = ttk.Frame(root)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.9, relheigth=0.6, anchor='n')
+
+label = ttk.Label(lower_frame, anchor="nw")
+label.place(relwidth=1, relheigth=1)
+
+root.mainloop()
 
